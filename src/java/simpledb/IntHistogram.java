d1edfad62d3873c37ddfd804d9dfb81521029eb4
Jonathan Ong <risingmidnight@ucla.edu>
2015-03-12 09:02:29 -0700

    Implemented OP

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -132,6 +132,132 @@ public class IntHistogram {
 
     	// some code goes here
         //return -1.0;
+        //OP can equal Predicate.Op.EQUALS
+        //*************************.GREATER_THAN
+        //*************************.GREATER_THAN_OR_EQ
+        //*************************.LESS_THAN
+        //*************************.LESS_THAN_OR_EQ
+        //*************************.NOT_EQUALS
+        
+        if (op == Predicate.Op.EQUALS){
+            //check if in range
+            if (v > this.max || v < this.max){
+                //no selectivity at all!
+                return 0.0;
+            }
+            else{
+                //convert V to bucket index and check that index
+                int index;
+    	        double tempd = (v - this.min)/bucket_size;
+    	        
+    	        index = (int)Math.round(tempd); //index needs to be int
+    	        if (index == buckets){
+    	            index -= 1;
+    	        }
+        	    //now we have hintogram[index]
+        	   //can be 0, can have width, 
+        	   //sel = height/width/total num
+        	   //diwth = right[index] - left[index]
+        	   if (bucket_size == 1){//width is 1
+        	        return (double)(hintogram[index]/values);
+        	        //   height
+        	        //   values
+        	   }
+        	   else{
+        	       int width = right[index] - left[index] +1;
+        	       return (double) (hintogram[index]/(width*values) );
+        	       //   height
+        	       //   width * values
+        	   }
+            }
+        }
+        else if (op == Predicate.Op.NOT_EQUALS){
+            //not equal means excluding value for V
+            //for histogram we can sum the values greather thana nd less than v
+            return (estimateSelectivity(Predicate.Op.GREATER_THAN, v) + estimateSelectivity(Predicate.Op.LESS_THAN, v));
+        }
+        else if (op == Predicate.Op.GREATER_THAN){
+            //check if in range
+            if (v < thix.min) return 1.0;
+            if (v > this.max) return 0.0;
+            //we have to know the largest value of bucket
+            //need right array and left array
+            int index;
+	        double tempd = ((double)(v - this.min))/(double)bucket_size;
+	        index = (int)Math.round(tempd); //index needs to be int
+	        if (index == buckets){
+	            index -= 1;
+	        }
+	        if (right[index] > v){
+	            double ans = 0.0;
+	            //starting from bucket for V, go through all buckets and aggreate selectivity
+                for (int i = index; i < this.buckets; i++){
+                    double bS = ((double) this.hintogram[i])/(double)values;
+                    
+                    int width;
+                    if (bucket_size == 1){
+                        width =1;
+                    }
+                    else{
+                        width = right[i]- left[i] +1;
+                    }
+                    //if v is within a bucket, we need to estimate within the bucket, use the right value
+                    double buck_sel = 1.0;
+                    if (v > this.lowest[i]){
+                        
+                        buck_sel = ((double) (this.right[i] - v)/(double) width);
+                    }
+                    ans += bS * buck_sel;
+                    
+                }
+                return ans;
+	        }
+	        else{
+	            //v is the greatest part of the histogram, we can include the whole thing
+	            //similar to previous code, but we start fro mthe next index
+	            double ans = 0.0;
+	            //starting from bucket for V, go through all buckets and aggreate selectivity
+                for (int i = index; i < this.buckets; i++){
+                    double bS = ((double) this.hintogram[i])/(double)values;
+                    
+                    int width;
+                    if (bucket_size == 1){
+                        width =1;
+                    }
+                    else{
+                        width = right[i]- left[i] +1;
+                    }
+                    //if v is within a bucket, we need to estimate within the bucket, use the right value
+                    double buck_sel = 1.0;
+                    if (v > this.lowest[i]){
+                        
+                        buck_sel = ((double) (this.right[i] - v)/(double) width);
+                    }
+                    ans += bS * buck_sel;
+                    
+                }
+                return ans;
+	            
+	        }
+            
+        }
+        else if (op == Predicate.OP.GREATER_THAN_OR_EQ){
+            //we ahve equals and greater than
+            //sum the two
+            return estimateSelectivity(Predicate.Op.EQUALS, v)+
+                        estimateSelectivity(Predicate.Op.GREATER_THAN, v);
+        }
+        else if (op == Predicate.OP.LESS_THAN){
+            //we have greater than or equal to 
+            return (1.0 - estimateSelectivity(Predicate.Op.GREATER_THAN_OR_EQ, v));
+        }
+        else if (op == Predicate.OP.LESS_THAN_OR_EQ){
+            //we have greater than
+            return (1.0 - estimateSelectivity(Predicate.Op.GREATER_THAN, v));
+            
+        }
+        
+        
     }
     
     /**
